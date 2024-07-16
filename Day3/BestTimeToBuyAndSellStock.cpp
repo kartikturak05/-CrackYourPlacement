c7fe41dfa92a0b85010538ce38e0e6bc0961d19a
Kartik Turak <kartikturak1@gmail.com>
2024-07-16 23:58:42 +0530

    Day 3

---
+++ Day3/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int mini = prices[0];
+    int profit = 0;
+    int cost =0;
+    for(int i=1;i<prices.size();i++){
+        cost = prices[i]-mini;
+        profit = max(cost,profit);
+        mini = min(mini,prices[i]);
+    }
+    return profit;
+    }
+};
